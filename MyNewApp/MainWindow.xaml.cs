3e57bdeef180c64a96921a1cdda9bdeeb2093158
Ahmed.Abdou <ahmedabduo625@gmail.com>
2024-08-09 16:40:47 +0300

    Add project files.

---
+++ MyNewApp/MainWindow.xaml.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace MyNewApp
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        //UpdateManager manage;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        private async void Button_Click(object sender, RoutedEventArgs e)
+        {
+            //manage = await UpdateManager.GitHubUpdateManager(@"https://github.com/AhmedAbduo625/DemoApp");
+            //var updateInfo = await manage.CheckForUpdate();
+            //if (updateInfo.ReleasesToApply.Count > 0)
+            //{
+            //    MessageBox.Show("New Release is available");
+            //    UpdateBtn.IsEnabled = true;
+            //}
+            //else
+            //{
+            //    MessageBox.Show("No Release available");
+            //}
+        }
+
+        private async void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            //await manage.UpdateApp();
+            //MessageBox.Show("Updated Successfully");
+        }
+    }
+}
